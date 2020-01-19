3e5fb4af6765baf53c3773bc88b4ec285453525a
Alberto <albertom359@gmail.com>
2020-01-19 19:22:26 +0100

    added potion drops
    Añadido drop de pociones de curación en el npc. Correcciones en otros puntos.
    Añadido layers adicionales

--- scripts/Enemies/Hound/Hound.gd
+++ scripts/Enemies/Hound/Hound.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 const HITS = 1
 const DAMAGE = 3
+const TOTAL_DIVIDOR = 100 #Divisor del random.
+const DROP = 7 #Número máximo para dropear
 
 #Función que se lanza al chocar con un cuerpo (En este caso el del jugador, único con el que interactuará)
 #Lanzamos el evento hitPlayer y pasamos el daño del NPC en si.
@@ -12,6 +14,7 @@ func _on_Area2D_body_entered(body):
 #Función para controlar la vida del HOUND en cuestión. Si el número de golpes recibidas es mayor al 
 #total de golpes que puede recibir, llamará a lafunción de drop y desaparecerá (morirá)
 func houndHitted(hits):
+	releaseDrop()
 	if hits > HITS:
 		releaseDrop()
 		queue_free()
@@ -21,4 +24,9 @@ func houndHitted(hits):
 
 #Función que se encarga de llamar al dropper central de los enemigos.		
 func releaseDrop():
-	get_tree().call_group("Enemies", "drop")
+	randomize()
+	var luckyKill = randi() % TOTAL_DIVIDOR
+	if luckyKill <= DROP :
+		var potion = load("res://scenes/Potions/HealPotion.tscn").instance()
+		potion.position.x = $".".position.x;
+		$".".get_parent().add_child(potion)
